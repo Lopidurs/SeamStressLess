08d74833842a31ecc84754003b3f588a5da48579
Lopidurs <nathan.sancke@hotmail.fr>
2024-08-01 11:13:33 +0200

    removeFile

--- storage/src/main/java/com/seamstressless/storage/utils/dto/PriceRecord/PriceRecordReq.java
+++ storage/src/main/java/com/seamstressless/storage/utils/dto/PriceRecord/PriceRecordReq.java
@@ -1,11 +1,6 @@
 package com.seamstressless.storage.utils.dto.PriceRecord;
 
-import com.seamstressless.storage.domain.entities.PriceRecord;
-import com.seamstressless.storage.domain.entities.Product;
-import com.seamstressless.storage.repository.ProductRepo;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 public record PriceRecordReq (
